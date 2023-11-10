54a92facb3095572b273eb65887c8e29d83f64dd
github-usman <usmantoyou@gmail.com>
2023-11-10 09:53:09 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Demo Application by usman Reactjs application
         </a>
       </header>
     </div>
